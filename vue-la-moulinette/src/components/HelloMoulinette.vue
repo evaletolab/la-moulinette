bfa755006658a566487efac8c9a10288a1876cf9
David Hodgetts <david.demainlalune@gmail.com>
2021-01-28 19:06:35 +0100

    adds vue variant

---
+++ vue-la-moulinette/src/components/HelloMoulinette.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1 v-if="visible">hello {{name}} </h1>
+</template>
+
+
+<script>
+export default {
+    name: 'HelloMoulinette',
+    props: {
+        name: String,
+        visible: Boolean
+    }
+}
+</script>
---
+++ vue-la-moulinette/src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloMoulinette v-bind:visible="vis" name="Moulinette"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import HelloMoulinette from '@/components/HelloMoulinette.vue';
+
+export default {
+  name: 'Home',
+  data: function(){
+    return {
+      vis:true
+    };
+  },
+  components: {
+    HelloWorld,
+    HelloMoulinette
+  },
+  mounted(){
+    console.log("we are mounted", this.vis);
+    setInterval(() => {
+      this.vis = !this.vis;
+    }
+      , 1000);
+  }
+}
+</script>
